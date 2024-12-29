44051bcd3e81ce6bf4faaa77dd343ce0e2a6f087
denyskotcode <kotdenis2004@gmail.com>
2024-12-29 02:07:48 +0100

    fix all memleaks

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 01:40:14 by dkot              #+#    #+#             */
-/*   Updated: 2024/12/29 01:45:48 by dkot             ###   ########.fr       */
+/*   Updated: 2024/12/29 02:05:12 by dkot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ void	check_first_and_last_line(int line_length, int fd, char *line);
 void	check_map_width(t_line *lines, t_map *map, int fd);
 void	process_line(t_line *lines, t_map *map, int fd);
 void	parse_map(const char *filename, t_map *map);
+void	free_gnl_buffer(void);
 char	*get_next_line(int fd); //temp variant
 
 #endif
