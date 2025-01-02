7bc90ab156ca4847280dce9eb7ac0d295bfa021f
Denis Kot <kotdenis2004@gmail.com>
2025-01-02 05:56:02 +0100

    fix all norms, now will do fix get_next_line

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: dkot <dkot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 01:40:14 by dkot              #+#    #+#             */
-/*   Updated: 2025/01/01 08:48:48 by dkot             ###   ########.fr       */
+/*   Updated: 2025/01/02 05:53:56 by dkot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,27 +75,29 @@ typedef struct s_game
 	t_render	*render;
 }				t_game;
 
-void	cleanup_textures(void *mlx, t_t *t);
-void	declare_texture(t_t *t, void *mlx, int tile_size);
-void	render_grid(t_render *render, t_map *map, t_player *player);
-void	declare_player(t_map *map, t_player *player);
-int		handle_keypress(int keycode, t_game *game);
-void	processing_moves(t_map *map, t_player *player, t_new_coord c,
-			t_render *render);
-int		close_window(t_map *map);
-void	handle_events(t_game *game);
-void	render_map(t_map *map);
-void	declare_map(t_map *map);
-void	declare_line(t_line *line);
-void	error_exit(char *message, char *line, char *last_line, int fd);
-void	check_t_map(t_map *map);
-void	process_line(t_line *lines, t_map *map, int fd);
-void	parse_map(const char *filename, t_map *map);
-void	check_middle_line(t_line *lines, t_map *map, int fd);
-void	check_first_and_last_line(int line_length, int fd, char *line);
-void	check_map_width(t_line *lines, t_map *map, int fd);
-void	allocation_map(const char *filename, t_map *map);
-void	free_map(t_map *map);
-char	*get_next_line(int fd);
+void		cleanup_textures(void *mlx, t_t *t);
+void		declare_texture(t_t *t, void *mlx, int tile_size);
+void		render_grid(t_render *render, t_map *map, t_player *player);
+void		declare_player(t_map *map, t_player *player);
+int			handle_keypress(int keycode, t_game *game);
+void		processing_moves(t_map *map, t_player *player, t_new_coord c,
+				t_render *render);
+int			close_window(t_map *map);
+void		handle_events(t_game *game);
+void		render_map(t_map *map);
+void		declare_map(t_map *map);
+void		declare_line(t_line *line);
+void		error_exit(char *message, char *line, char *last_line, int fd);
+void		check_t_map(t_map *map);
+void		process_line(t_line *lines, t_map *map, int fd);
+void		parse_map(const char *filename, t_map *map);
+void		check_middle_line(t_line *lines, t_map *map, int fd);
+void		check_first_and_last_line(int line_length, int fd, char *line);
+void		check_map_width(t_line *lines, t_map *map, int fd);
+void		allocation_map(const char *filename, t_map *map);
+void		free_map(t_map *map);
+char		*get_next_line(int fd);
+static void	init_game(t_map *map,
+				t_game *game, t_render *render, t_player *player);
 
 #endif
